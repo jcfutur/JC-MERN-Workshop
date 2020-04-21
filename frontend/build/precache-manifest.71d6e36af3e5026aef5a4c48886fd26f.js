944f6ceca9ed116cd19aa2d3845e5770aeb7c3b1
jcfutur <jean.camard@gmail.com>
2020-04-21 18:45:46 +0200

    commit 5

---
+++ frontend/build/precache-manifest.71d6e36af3e5026aef5a4c48886fd26f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75837a0b9ce5e209e20316e7c574b5ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dff6761b846149f18f37",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "2c1828ddfebb1b1ac356",
+    "url": "/static/js/2.9fdd91a8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9fdd91a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dff6761b846149f18f37",
+    "url": "/static/js/main.d8ece8e2.chunk.js"
+  },
+  {
+    "revision": "700d31d1c6f37b7bdc51",
+    "url": "/static/js/runtime-main.84578d5e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
